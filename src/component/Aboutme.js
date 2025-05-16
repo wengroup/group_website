0d3b114b9594632381af57a609e2b104d58eb033
minhe <mmbliv@gmail.com>
2025-05-16 16:05:01 +0800

    before using ai

--- src/component/Aboutme.js
+++ src/component/Aboutme.js
@@ -31,16 +31,24 @@ export const Aboutme = () => {
   return (
     <article id="about" className="sm:col-span-2 pt-16">
       {/* this is the about me div */}
-      <div className="flex flex-col md:flex-row items-center gap-8 md:gap-48 max-w-4xl mx-auto p-6">
+      <div className="flex flex-col md:flex-row items-start md:gap-48 max-w-4xl mx-auto p-6 gap-32">
         {/* <h2 className="text-center"></h2> */}
         {/* there used to be an `About` text above; we've removed it */}
-        <div className=" w-40 h-40 md:w-60 md:h-60 flex-shrink-0">
-          <GatsbyImage
-            image={data.file.childrenImageSharp[0].gatsbyImageData}
-            alt="wengroup"
-            className=" rounded-full overflow-hidden w-full h-full object-cover border-4 border-gray-100 shadow-md"
-          />
+        <div className="flex flex-col items-center w-full md:w-auto">
+          <div className=" w-40 h-40 md:w-60 md:h-60 flex-shrink-0 ">
+            <GatsbyImage
+              image={data.file.childrenImageSharp[0].gatsbyImageData}
+              alt="wengroup"
+              className=" rounded-full overflow-hidden w-full h-full object-cover border-4 border-gray-100 shadow-md"
+            />
+
+            <div className="flex gap-3 justify-center -mt-9">
+              {/* <div className="flex gap-4 -mt-2"> */}
+              <Publication />
+            </div>
+          </div>
         </div>
+
         <div className="flex-1">
           <p>
             <div dangerouslySetInnerHTML={{ __html: about[0].html }} />
@@ -50,7 +58,7 @@ export const Aboutme = () => {
         </div>
       </div>
       {/* this is the publication div */}
-      <Publication />
+      {/* <Publication /> */}
     </article>
   );
 };
--- src/component/Publication.js
+++ src/component/Publication.js
@@ -40,41 +40,41 @@ export const Publication = () => {
 
   return (
     <div className="pt-24" id="publications">
-      <h2 className="text-center">PUBLICATIONS</h2>
-      <div className="flex flex-row justify-center mb-10">
-        {data.subTitle.childrenImageSharp === [] ? (
+      {/* <h2 className="text-center">PUBLICATIONS</h2> */}
+      {/* <div className="flex flex-row justify-center mb-10">
+        {data.subTitle.childrenImageSharp == [] ? (
           <SubtitleIcon icon={data.subTitle.childrenImageSharp} smaller />
         ) : (
           <SubtitleIconSvg icon={data.subTitle.publicURL} smaller />
         )}
-      </div>
-      <div className="flex flex-col sm:flex-row gap-6 justify-center text-lg w-max mx-auto">
+      </div> */}
+      <div className="flex gap-6 justify-center text-lg w-max mx-auto">
         <a
           href="https://scholar.google.com/citations?user=RXLH5k4AAAAJ&hl=en"
           className="hover:text-Button hover:underline flex items-center"
         >
-          <div className="w-6 h-6">
+          <div className="w-8 h-8">
             <img src={data.google.publicURL} alt="googlescholar" />
           </div>
-          <p className="indent-2">Google Scholar</p>
+          {/* <p className="indent-2">Google Scholar</p> */}
         </a>
         <a
           href="https://www.researchgate.net/profile/Mingjian-Wen"
           className="hover:text-Button hover:underline flex space-x-1 items-center "
         >
-          <div className="w-6 h-6">
+          <div className="w-8 h-8">
             <img src={data.researchGate.publicURL} alt="researchgate" />
           </div>
-          <p className="indent-2">ResearchGate</p>
+          {/* <p className="indent-2">ResearchGate</p> */}
         </a>
         <a
           href="https://orcid.org/0000-0003-0013-575X"
           className="hover:text-Button hover:underline flex space-x-1 items-center "
         >
-          <div className="w-6 h-6">
+          <div className="w-8 h-8">
             <img src={data.orcid.publicURL} alt="orcid" />
           </div>
-          <p className="indent-2">ORCID</p>
+          {/* <p className="indent-2">ORCID</p> */}
         </a>
       </div>
     </div>
