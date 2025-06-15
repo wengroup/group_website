ea283c54d006322ebebfa926eae5fe2cb69e3492
minhe <mmbliv@gmail.com>
2025-06-15 23:09:24 +0800

    research done

--- src/component/ResearchCard.js
+++ src/component/ResearchCard.js
@@ -4,6 +4,15 @@ import { GatsbyImage } from "gatsby-plugin-image";
 const ResearchCard = ({ item, imgs }) => {
   return (
     <div key={item.id} className="pb-6 flex w-full gap-9">
+      <div className="w-2/3">
+        <p className="pb-4 text-base font-semibold" id={item.frontmatter.title}>
+          {item.frontmatter.title}
+        </p>
+
+        <p className="pb-4">
+          <div dangerouslySetInnerHTML={{ __html: item.html }} />
+        </p>
+      </div>
       <div className=" w-1/3 items-center flex">
         {imgs.map((p) => {
           if (p.relativePath === item.frontmatter.img) {
@@ -19,15 +28,6 @@ const ResearchCard = ({ item, imgs }) => {
             return null;
           }
         })}
-      </div>
-      <div className="w-2/3">
-        <p className="pb-4 text-base font-semibold" id={item.frontmatter.title}>
-          {item.frontmatter.title}
-        </p>
-
-        <p className="pb-4">
-          <div dangerouslySetInnerHTML={{ __html: item.html }} />
-        </p>
       </div>
 
       <hr />
