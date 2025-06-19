6020a6fcdd9251177e2aca4c1e9e5854e004b375
minhe <mmbliv@gmail.com>
2025-06-19 21:28:39 +0800

    go back

--- src/component/Research.js
+++ src/component/Research.js
@@ -34,33 +34,29 @@ export const Research = ({ researches, showLink }) => {
     allFile: { nodes: imgs },
   } = data;
   return (
-    <div className="flex items-center justify-center flex-col">
-      <article
-        id="research"
-        className="sm:w-3/4 "
-
-        // className={`${
-        //   showLink
-        //     ? "px-14 sm:w-4/5 mx-auto max-w-5xl sm:pt-20"
-        //     : "px-14 sm:w-4/5 mx-auto max-w-5xl sm:pt-20"
-        // }`}
-      >
-        <h2 className="text-center mb-20 pt-20">
-          {showLink ? <Link to="Researches">Research</Link> : "Research"}
-        </h2>
-        {/* <div className="flex flex-row justify-center mb-10">
+    <article
+      id="research"
+      className={`${
+        showLink
+          ? "px-14 sm:w-4/5 mx-auto max-w-5xl sm:pt-20"
+          : "px-14 sm:w-4/5 mx-auto max-w-5xl sm:pt-20"
+      }`}
+    >
+      <h2 className="text-center">
+        {showLink ? <Link to="Researches">Research</Link> : "Research"}
+      </h2>
+      <div className="flex flex-row justify-center mb-10">
         {data.file.childrenImageSharp == [] ? (
           <SubtitleIcon icon={data.file.childrenImageSharp} />
         ) : (
           <SubtitleIconSvg icon={data.file.publicURL} />
         )}
-      </div> */}
-        <ul className="grid gap-12">
-          {researches.map((item) => {
-            return <ResearchCard item={item} key={item.id} imgs={imgs} />;
-          })}
-        </ul>
-      </article>
-    </div>
+      </div>
+      <ul className="grid gap-5">
+        {researches.map((item) => {
+          return <ResearchCard item={item} key={item.id} imgs={imgs} />;
+        })}
+      </ul>
+    </article>
   );
 };
--- src/component/ResearchCard.js
+++ src/component/ResearchCard.js
@@ -3,20 +3,15 @@ import { GatsbyImage } from "gatsby-plugin-image";
 
 const ResearchCard = ({ item, imgs }) => {
   return (
-    <div
-      key={item.id}
-      className="pb-6 flex w-full gap-9 sm:flex-row flex-col items-center justify-center "
-    >
-      <div className="sm:w-2/3 w-full flex flex-col items-center justify-center sm:items-start">
-        <p className="pb-4 text-base font-semibold" id={item.frontmatter.title}>
-          {item.frontmatter.title}
-        </p>
+    <div key={item.id} className="pb-6 w-full ">
+      <p className="subTitle pb-4" id={item.frontmatter.title}>
+        {item.frontmatter.title}
+      </p>
 
-        <p className="pb-4 px-10 sm:px-0">
-          <div dangerouslySetInnerHTML={{ __html: item.html }} />
-        </p>
-      </div>
-      <div className=" sm:w-1/2 items-center flex w-3/4 justify-center ">
+      <p className="pb-4">
+        <div dangerouslySetInnerHTML={{ __html: item.html }} />
+      </p>
+      <div className="">
         {imgs.map((p) => {
           if (p.relativePath === item.frontmatter.img) {
             return (
@@ -25,14 +20,13 @@ const ResearchCard = ({ item, imgs }) => {
               //   alt={item.frontmatter.title}
               //   className="w-1/2 mb-4"
               // />
-              <img src={p.publicURL} className=" w-full "></img>
+              <img src={p.publicURL} className=" w-1/2 mb-4 mx-auto"></img>
             );
           } else {
             return null;
           }
         })}
       </div>
-
       <hr />
     </div>
   );
