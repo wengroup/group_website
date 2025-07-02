77e04a674d2b3f2082196f283919977052920d46
minhe <mmbliv@gmail.com>
2025-07-02 08:40:29 +0800

    nav font

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -13,7 +13,7 @@ export const Nav = () => {
   const [activeItem, setActiveItem] = useState(); // 默认选中 "about"
   return (
     <>
-      <div className="fixed top-0 w-full h-14 z-50 bg-gray-200 border-b-[3px] border-transparent shadow-md">
+      <div className="fixed top-0 w-full h-14 z-50 bg-gray-200 border-b-[3px] border-transparent shadow-md text-lg">
         <ul className="hidden sm:flex justify-end w-5/6 max-w-6xl items-center h-full gap-2 mx-auto pr-0">
           <li className="mr-auto text-xl font-bold">WEN GROUP</li>
           <div className="flex gap-5 font-medium">
