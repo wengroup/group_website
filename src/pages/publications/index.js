aeb61a2ada23ca7639959341141da6c3c5c3795b
minhe <mmbliv@gmail.com>
2025-09-18 16:40:48 +0800

    publication order and space

--- src/pages/publications/index.js
+++ src/pages/publications/index.js
@@ -5,7 +5,7 @@ import { useStaticQuery, graphql } from "gatsby";
 import { convertMarkdownToPublications } from "../../utils/convertMDToArray";
 const query = graphql`
   {
-    allReference(sort: { fields: year }) {
+    allReference(sort: { fields: year, order: DESC }) {
       edges {
         node {
           key
@@ -54,41 +54,53 @@ const Publications = () => {
                 counterReset: "item " + (papers.length + 1),
               }}
             >
-              {papers.map((item) => (
-                <li
-                  key={item.node.id}
-                  className="mb-4 text-left text-lg relative"
-                  style={{
-                    counterIncrement: "item -1",
-                  }}
-                >
-                  <span className="absolute -left-6">
-                    {papers.length - papers.indexOf(item)}.{" "}
-                  </span>
-
-                  <a
-                    href={item.node.file?.publicURL || "#"}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="paper-link hover:underline font-medium"
+              {papers.map((item, idx) => {
+                const number = papers.length - idx; // 使用 map 的 index，简单可靠
+                return (
+                  <li
+                    key={item.node.id}
+                    className="mb-4 text-left text-lg relative"
+                    style={{
+                      counterIncrement: "item -1",
+                    }}
                   >
-                    {item.node.title}
-                  </a>
-                  <span className="text-gray-700">. {item.node.author}. </span>
-                  <span className="italic font-semibold">
-                    {item.node.journal}
-                  </span>
-                  {item.node.volume && (
-                    <span className="text-gray-700">, {item.node.volume}</span>
-                  )}
-                  {item.node.pages && (
-                    <span className="text-gray-700">, {item.node.pages}</span>
-                  )}
-                  {item.node.year && (
-                    <span className="text-gray-700">, {item.node.year}.</span>
-                  )}
-                </li>
-              ))}
+                    <span className="absolute -left-6 inline-block text-right w-11">
+                      {number}.
+                    </span>
+                    <div className="ml-7">
+                      <a
+                        href={item.node.file?.publicURL || "#"}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="paper-link hover:underline font-medium"
+                      >
+                        {item.node.title}
+                      </a>
+                      <span className="text-gray-700">
+                        . {item.node.author}.{" "}
+                      </span>
+                      <span className="italic font-semibold">
+                        {item.node.journal}
+                      </span>
+                      {item.node.volume && (
+                        <span className="text-gray-700">
+                          , {item.node.volume}
+                        </span>
+                      )}
+                      {item.node.pages && (
+                        <span className="text-gray-700">
+                          , {item.node.pages}
+                        </span>
+                      )}
+                      {item.node.year && (
+                        <span className="text-gray-700">
+                          , {item.node.year}.
+                        </span>
+                      )}
+                    </div>
+                  </li>
+                );
+              })}
             </ol>
           </div>
         </div>
