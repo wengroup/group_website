25e0da33e6914be9f75355ef8c8c653c727d2543
Mingjian Wen <wenxx151@gmail.com>
2025-11-17 21:50:12 +0800

    Revert back

--- src/component/SEO.js
+++ src/component/SEO.js
@@ -2,59 +2,48 @@ import React from "react";
 import { Helmet } from "react-helmet";
 
 const Seo = ({ title, description, image, url, lang }) => {
-  const defaultTitle = "文明健课题组 - 电子科大 ｜ Wen Group, UESTC";
-  const defaultDescription = "电子科技大学文明健教授课题组, 主要研究方向为数据和人工智能驱动的材料设计，包括材料基因组、机器学习算法、不确定性量化、能源及磁性材料等领域的交叉研究。";
+  const defaultTitle = "电子科大文明健 - Wen Group";
+  const defaultDescription =
+    "电子科技大学文明健教授课题组，专注于核材料与工程研究，展示科研成果、团队信息与最新动态。";
   const defaultImage = "https://wengroup.github.io/preview.png";
   const defaultUrl = "https://wengroup.github.io/";
   const defaultLang = "en";
 
-  const seoTitle = title || defaultTitle;
-  const seoDescription = description || defaultDescription;
-  const seoImage = image || defaultImage;
-  const seoUrl = url || defaultUrl;
-  const seoLang = lang || defaultLang;
-
   return (
     <Helmet
-      htmlAttributes={{ lang: seoLang }}
-      title={seoTitle}
+      htmlAttributes={{ lang: lang || defaultLang }}
+      title={title || defaultTitle}
     >
-      <meta name="description" content={seoDescription} />
-      <meta name="keywords" content="人工智能材料设计, 机器学习势函数, 材料基因组, 电池材料, 磁性材料, 密度泛函理论, 分子动力学, 电子科技大学, 电子科大,  基础与前沿研究院, 文明健, Mingjian Wen Group UESTC" />
-      <meta name="author" content="文明健, Mingjian Wen Group" />
+      <meta name="description" content={description || defaultDescription} />
+      <meta name="keywords" content="电子科大,文明健, Wen Group" />
+      <meta name="author" content="文明健, Wen Group" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-      {/* Open Graph */}
-      <meta property="og:title" content={seoTitle} />
-      <meta property="og:description" content={seoDescription} />
+      <meta property="og:title" content={title || defaultTitle} />
+      <meta
+        property="og:description"
+        content={description || defaultDescription}
+      />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content={seoUrl} />
-      <meta property="og:image" content={seoImage} />
-      <meta property="og:site_name" content="文明健课题组 - Mingjian Wen Group UESTC" />
+      <meta property="og:url" content={url || defaultUrl} />
+      <meta property="og:image" content={image || defaultImage} />
 
-      {/* Canonical Link */}
-      <link rel="canonical" href={seoUrl} />
-
-      {/* Schema.org */}
       <script type="application/ld+json">
         {`
         {
           "@context": "https://schema.org",
-          "@type": "ResearchLab",
-          "name": "文明健课题组 - Mingjian Wen Group",
-          "description": "文明健课题组 - 电子科大 ｜ Wen Group, UESTC",
-          "url": "https://wengroup.github.io",
+          "@type": "Person",
+          "name": "文明健",
           "affiliation": {
             "@type": "CollegeOrUniversity",
-            "name": "电子科技大学, UESTC",
-            "url": "https://www.uestc.edu.cn"
+            "name": "电子科技大学"
           },
-          "founder": {
-            "@type": "Person",
-            "name": "文明健, Mingjian Wen",
-            "jobTitle": "Professor",
-          },
-          "keywords": "人工智能材料设计, 机器学习势函数, 材料基因组, 电池材料, 磁性材料",
+          "jobTitle": "Professor",
+          "url": "https://wengroup.github.io/",
+          "worksFor": {
+            "@type": "ResearchProject",
+            "name": "Wen Group"
+          }
         }
         `}
       </script>
